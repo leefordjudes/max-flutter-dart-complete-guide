91245cf131f890674c6bd0abea1df976b8155c20
leefordjudes <leefordjudes@gmail.com>
2024-02-18 22:18:09 +0530

    understanding about keys, var, final, const

--- 6.flutter_internals/lib/main.dart
+++ 6.flutter_internals/lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_internals/keys/keys.dart';
 
-import 'package:flutter_internals/ui_updates_demo.dart';
+// import 'package:flutter_internals/ui_updates_demo.dart';
 
 void main() {
+  final mutatable = [1, 2, 3];
+  mutatable.add(4);
+  // mutatable = [1,2,3,4,5]; // can not reassign
+
+  const _unmodifiable = [1, 2, 3];
+  // unmodifiable.add(4); // shows run time error
+
+  var reassignable = [1, 2, 3];
+  reassignable.add(4);
+  reassignable = [4, 3, 2, 1];
+
   runApp(const App());
 }
 
@@ -14,11 +26,11 @@ class App extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(useMaterial3: true),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Internals'),
-        ),
-        body: const UIUpdatesDemo(),
-      ),
+          appBar: AppBar(
+            title: const Text('Flutter Internals'),
+          ),
+          // body: const UIUpdatesDemo(),
+          body: const Keys()),
     );
   }
 }
